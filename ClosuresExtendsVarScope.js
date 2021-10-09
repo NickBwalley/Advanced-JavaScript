8ca449356da5d9fd07181d9345596bf9dd228cd6
NickBwalley <nickbwalley@gmail.com>
2021-10-09 23:15:50 +0300

    closures to extends variable scope

--- ClosuresExtendsVarScope.js
+++ ClosuresExtendsVarScope.js
@@ -1,14 +1,15 @@
 //closures allow variable scope to be extended past the common scope restrictions of functions.
 // To create a closure, you nest a function inside of a function. That inner function has access to all variables in its parent function’s scope.
 function myObject() {
-  this.property1 = "red";
-  this.property2 = "blue";
-  var newValue = this.property1;
-  this.performMethod = () => {
-    myMethodValue = newValue;
-    return myMethodValue;
+  this.property1 = "first property";
+  this.property2 = "second property";
+  var finalProperty = this.property1;
+
+  this.myObject2 = () => {
+    myProperty = finalProperty;
+    return myProperty;
   };
 }
 
-var myObjectInstance = new myObject();
-console.log(myObjectInstance.performMethod());
+var name1 = new myObject();
+console.log(name1.myObject2());
