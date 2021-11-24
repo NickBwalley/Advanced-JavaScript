96ced0f2f50dca0de0b9e5ac4e9d5ced26a5e770
NickBwalley <nickbwalley@gmail.com>
2021-11-24 07:37:27 +0300

    big O Notation

--- findMissingNumber.js
+++ findMissingNumber.js
@@ -1,19 +1,20 @@
-var myArray = [6, 2, 1, 3];
-console.log(myArray);
+var myArray = [6, 2, 1, 3]
+console.log(myArray)
 
 function findMissingNumber(myArray) {
-  let missingNumber = [];
+  let missingNumber = []
 
-  let minNum = Math.min(...myArray); // finds the minimum number in the array
-  let maxNum = Math.max(...myArray); // finds the maximum number in the array
+  let minNum = Math.min(...myArray) // finds the minimum number in the array
+  let maxNum = Math.max(...myArray) // finds the maximum number in the array
 
   for (let i = minNum; i < maxNum; i++) {
     if (myArray.indexOf(i) < 0) {
       // indexOf returns -1 if it finds the element [i] is missing so it pushes in to our newly defined array missingNumber[]
-      missingNumber.push(i);
+      missingNumber.push(i)
+      missingNumber.push('i')
     }
   }
-  return missingNumber;
+  return missingNumber
 }
 
-console.log(findMissingNumber(myArray));
+console.log(findMissingNumber(myArray))
